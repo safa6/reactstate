0255a3b665df02ca1d87737abf6c533d344f9303
safa6 <safaaloui69@gmail.com>
2021-03-11 12:36:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+class App extends Component {
+  state = {
+    Person:{
+      fullName:"safa aloui",
+      bio: "nothing" ,
+      imgSrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIIK1QdUGfhbHISs4Zymz9TxS6IJjNxp0JRQ&usqp=CAU" , 
+      profession: "nothing for now", 
+    },
+    isShow:true,
+    count:0,
+  }
+  handelhide=()=>this.setState({isShow : !this.state.isShow, count:0})
+  componentDidMount () {
+    setInterval(()=>{
+     this.setState(count => ({count:this.state.count +1}));
+     } , 1000 )
+  };
+  
+  render() {
+    return (
+      <div className="personne">
+        <button onClick={()=> this.handelhide()} > {this.state.isShow ? "Hide" :"Show" } </button>
+        { this.state.isShow ? (
+        <>
+        <h2>{this.state.Person.fullName}</h2>
+        <h4>{this.state.Person.bio}</h4>
+        <h4> {this.state.Person.profession}</h4>
+        <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIIK1QdUGfhbHISs4Zymz9TxS6IJjNxp0JRQ&usqp=CAU"} alt="profile" />
+        </>) : null}
+        {this.state.isShow ? 
+        <p>{this.state.count}</p>
+        : null }
+      </div>
+    )
+  }
 }
 
+ 
 export default App;
